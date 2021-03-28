a6b545551bb7d23b8c4debb18a0bb6d37d4c233d
Azin Khan <azin@azinkhan.com>
2021-03-28 13:01:13 +0100

    Use images for external social links

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 
 function addNav() {
-    var elem = document.getElementById("main_nav");
+    var navbar = document.getElementById("main_nav");
     var nav = [
         {
             href: "bio.html",
@@ -26,24 +26,6 @@ function addNav() {
             href: "https://mailchi.mp/dd26f6cec045/join-our-mailing-list",
             name: "SUBSCRIBE"
         },
-        {
-
-            href: "https://www.facebook.com/automaticpanic",
-            name: "FACEBOOK"
-        },
-        {
-            href: "https://soundcloud.com/automaticpanic",
-            name: "SOUNDCLOUD"
-        },
-        {
-            href: "https://www.instagram.com/automaticpanicband",
-            name: "INSTAGRAM"
-        },
-        {
-
-            href: "https://open.spotify.com/artist/0lzo8cm8bdkSzHCai6QdgK",
-            name: "SPOTIFY"
-        }
     ]
 
     nav.forEach(element => {
@@ -51,6 +33,47 @@ function addNav() {
         link.href = element.href;
         link.innerText = element.name;
         link.type = "button";
-        elem.appendChild(link);
+        navbar.appendChild(link);
     });
+
+    var images = [
+        {
+            href: "https://www.facebook.com/automaticpanic",
+            title: "Facebook",
+            image: "https://upload.wikimedia.org/wikipedia/commons/f/ff/Facebook_logo_36x36.svg"
+        },
+        {
+            href: "https://soundcloud.com/automaticpanic",
+            title: "Soundcloud",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Antu_soundcloud.svg/72px-Antu_soundcloud.svg.png"
+        },
+        {
+            href: "https://www.instagram.com/automaticpanicband",
+            title: "Instagram",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/72px-Instagram_logo_2016.svg.png"
+        },
+        {
+            href: "https://open.spotify.com/artist/0lzo8cm8bdkSzHCai6QdgK",
+            title: "Spotify",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/72px-Spotify_logo_without_text.svg.png"
+        }
+    ]
+
+    images.forEach(element => {
+        var link = document.createElement("a");
+        link.href = element.href;
+        link.title = element.title;
+        
+        var img = new Image();
+        img.src = element.image;
+        img.alt = element.title;
+        // Setting this in css doesn't work
+        img.width = 30;
+        img.height = 30;
+        
+        link.appendChild(img);
+        navbar.appendChild(link); 
+    });
+
+    content.appendChild(navbar);
 }
